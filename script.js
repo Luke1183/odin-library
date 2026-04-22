331556813dde1cda93ae3805c7f11e26fe5267ad
Luke <141526416+Luke1183@users.noreply.github.com>
2026-04-22 16:44:22 +0200

    Add function to add books to library

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function Book(title, author, genre) {
   if (!new.target) {
     throw Error("Use 'new'!");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.genre = genre;
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, genre) {
+  book = new Book(title, author, genre);
+  myLibrary.push(book);
+  return;
 }
