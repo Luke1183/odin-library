30ea7fb6ed3eff1324ce3ea4eec93456881e117b
Luke <141526416+Luke1183@users.noreply.github.com>
2026-04-22 11:18:47 +0200

    Add constructor for Book ojbject

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("weeyooo");
+const myLibrary = [];
+
+function Book(title, author, genre) {
+  if (!new.target) {
+    throw Error("Use 'new'!");
+  }
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
